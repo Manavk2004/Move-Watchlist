5d100a2cf2a5f9992b2319efa51f86bc781c2288
Manavk2004 <mjkamdar04@gmail.com>
2025-06-26 15:01:30 -0400

    Navbar initial

---
+++ index.js
@@ -0,0 +1,11 @@
+// let img = ""
+// fetch("http://www.omdbapi.com/?t=mission+impossible+ghost-protocol&apikey=4c61f6fa")
+//     .then(response => response.json())
+//     .then(data => {
+//         img = data.Poster
+//         console.log(data)
+//         document.getElementById("image-container").innerHTML = `<img src=${img}/>`
+//     })
+
+
+
