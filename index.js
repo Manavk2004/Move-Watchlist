1518409c775bf8aae01d139f82d7ce54296f533a
Manavk2004 <mjkamdar04@gmail.com>
2025-06-27 11:16:21 -0400

    Search bar fetching api

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-// let img = ""
-// fetch("http://www.omdbapi.com/?t=mission+impossible+ghost-protocol&apikey=4c61f6fa")
-//     .then(response => response.json())
-//     .then(data => {
-//         img = data.Poster
-//         console.log(data)
-//         document.getElementById("image-container").innerHTML = `<img src=${img}/>`
-//     })
+
+let searchBarWord = ""
+let alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+document.getElementById("search-bar").addEventListener("keydown", (event)=>{
+    console.log(event)
+    if (event.key === "Backspace"){
+        searchBarWord = searchBarWord.slice(0, -1)
+        console.log(searchBarWord)
+    }else if(alphabet.includes(event.key)){
+        searchBarWord += event.key
+        console.log(searchBarWord)
+    }else if (event.key === " "){
+        searchBarWord += " "
+        console.log(searchBarWord)
+    }else{
+        ""
+    }
+})
+
+document.getElementById("search-bar").addEventListener("input", (event)=>{
+    console.log(event)
+})
 
 
-
+document.getElementById("search-button").addEventListener("click", ()=>{
+    let searchableWord = searchBarWord.replace(/ /g, "+").toLowerCase()
+    console.log(searchableWord)
+    fetch(`http://www.omdbapi.com/?t=${searchableWord}&apikey=4c61f6fa`)
+        .then(response => response.json())
+        .then(data => {
+            img = data.Poster
+            console.log(data)
+            // document.getElementById("image-container").innerHTML = `<img src=${img}/>`
+        })
+    searchBarWord = ""
+    document.getElementById("search-bar").value = ""
+})
