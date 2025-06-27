2c2942d82cc0f33d452a5e712747dbdf8f666dd2
Manavk2004 <mjkamdar04@gmail.com>
2025-06-27 17:00:55 -0400

    Initial render

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ document.getElementById("search-bar").addEventListener("keydown", (event)=>{
     console.log(event)
     if (event.key === "Backspace"){
         searchBarWord = searchBarWord.slice(0, -1)
-        console.log(searchBarWord)
+
     }else if(alphabet.includes(event.key)){
         searchBarWord += event.key
-        console.log(searchBarWord)
+
     }else if (event.key === " "){
         searchBarWord += " "
-        console.log(searchBarWord)
+  
     }else{
         ""
     }
@@ -21,16 +21,30 @@ document.getElementById("search-bar").addEventListener("input", (event)=>{
     console.log(event)
 })
 
-
+let count = 0
 document.getElementById("search-button").addEventListener("click", ()=>{
     let searchableWord = searchBarWord.replace(/ /g, "+").toLowerCase()
-    console.log(searchableWord)
-    fetch(`http://www.omdbapi.com/?t=${searchableWord}&apikey=4c61f6fa`)
+
+    fetch(`http://www.omdbapi.com/?s=${searchableWord}&apikey=4c61f6fa`)
         .then(response => response.json())
         .then(data => {
-            img = data.Poster
-            console.log(data)
-            // document.getElementById("image-container").innerHTML = `<img src=${img}/>`
+
+            if(count === 0){
+                const element = document.getElementById("empty-logo-container")
+                element.remove()
+            }else{
+                document.getElementById("template-container").innerHTML = ""
+            }
+            count += 1
+            document.getElementById("template-container").style.height = "80%"
+            let arrayofMovies = data.Search
+            for (const movie of arrayofMovies){
+                const template = document.getElementById("template")
+                const clone = template.content.cloneNode(true)
+                const img = clone.querySelector("img")
+                img.src = movie.Poster
+                document.getElementById("template-container").appendChild(clone)
+            }
         })
     searchBarWord = ""
     document.getElementById("search-bar").value = ""
